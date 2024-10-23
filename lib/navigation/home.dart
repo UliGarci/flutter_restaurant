dd94fec08e83b1e962b7ebd74b1fb738aeac8b7f
Daniel-GM20 <ulisesgm.dev@gmail.com>
2024-10-23 16:16:35 -0600

    Add. get user firebase

--- lib/navigation/home.dart
+++ lib/navigation/home.dart
@@ -1,23 +1,48 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final db = FirebaseFirestore.instance;
+  @override
+  void initState() {
+    super.initState();
+    (() async => {
+          await db.collection("restaurants").get().then((event) {
+            for (var doc in event.docs) {
+              print("${doc.id} => ${doc.data()}");
+            }
+          })
+        })();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Inicio'),),
+      appBar: AppBar(
+        title: const Text('Inicio'),
+      ),
       body: Center(
         child: Column(
           children: [
             const Text('Bienvenido'),
-            const SizedBox(height: 32,),
-            ElevatedButton(onPressed: ()async{
-              await FirebaseAuth.instance.signOut();
-              Navigator.pushReplacementNamed(context, '/');
-              print('Cerrando sesión');
-            }, child: const Text('Cerrar sesión'))
+            const SizedBox(
+              height: 32,
+            ),
+            ElevatedButton(
+                onPressed: () async {
+                  await FirebaseAuth.instance.signOut();
+                  Navigator.pushReplacementNamed(context, '/');
+                  print('Cerrando sesión');
+                },
+                child: const Text('Cerrar sesión'))
           ],
         ),
       ),
